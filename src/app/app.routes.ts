cc07e6e659625300202fb3d03227bc6eb210c6bb
Eduardo <farid.merino1673@gmail.com>
2024-03-06 15:03:12 -0500

    Version 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { AboutMeComponent } from './body/about-me/about-me.component';
+import { EducationComponent } from './body/education/education.component';
+import { ContactComponent } from './body/contact/contact.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {
+      path: '',
+      redirectTo: 'about-me',
+      pathMatch: 'full'
+    },
+    {
+      path: 'about-me',
+      
+      children: [
+        { path: '', component: AboutMeComponent },
+      ]
+    },
+    {
+      path: 'education',
+      children: [
+        { path: '', component: EducationComponent },
+      ] 
+    },
+    {
+      path: 'contact',
+      children: [
+        { path: '', component: ContactComponent },
+      ]
+    }
+];
